eefe39f3408723bd0d2fe2c44848a73686e5a669
Federico Skliarevsky <federicoskliarevsky@gmail.com>
2020-10-07 22:01:06 -0300

    Minor view changes

--- Views/nav.php
+++ Views/nav.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
      <span class="navbar-text">
-          <strong>Framework</strong>
+          <strong>MoviePass</strong>
      </span>
      <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Agregar Alumno</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Cinema/showAddView">Agregar Cine</a>
           </li>
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">Listar Alumnos</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Cinema/showListView">Listar Cines</a>
           </li>          
      </ul>
 </nav>
