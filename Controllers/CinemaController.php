c9a6016c82b6a72e5363f6ab20ac6c1a53057c9b
Federico Skliarevsky <federicoskliarevsky@gmail.com>
2020-10-12 21:06:18 -0300

    New validations by name on cinema edit

--- Controllers/CinemaController.php
+++ Controllers/CinemaController.php
@@ -13,27 +13,16 @@
             $this-> cinemaDAO = new CinemaDAO();
         }
 
-        function validateCinemaName($name){
-            $cinemasList = $this-> cinemaDAO-> GetAll();
-            foreach ($cinemasList as $cinema){
-                if ($cinema["name"] === $name){
-                    return true;
-                }
-            }
-            return false;
-        }
-
         function addCinema($name, $ticketValue) {
             if ($this-> validateCinemaName($name)){
                 $this-> showAddView("Nombre de cine ya existente. Intente con otro.");
+            } else {
+                $cinema = new Cinema();
+                $cinema-> setName($name);
+                $ticketValue>0 ? $cinema-> setTicketValue($ticketValue) : $this-> showAddView("Try using a positive ticket value.");
+                $this-> cinemaDAO-> Add($cinema);
+                $this-> showListView();
             }
-
-            /**No neccesity to use an else sentence here */
-            $cinema = new Cinema();
-            $cinema-> setName($name);
-            $ticketValue>0 ? $cinema-> setTicketValue($ticketValue) : $this-> showAddView("Try using a positive ticket value.");
-            $this-> cinemaDAO-> Add($cinema);
-            $this-> showListView();
         }
 
         function removeCinema($id){
@@ -43,11 +32,19 @@
         }
 
         function editCinema ($id, $name, $ticketValue){
-            $cinema = new Cinema();
-            $cinema-> setName($name);
-            $ticketValue>0 ? $cinema-> setTicketValue($ticketValue) : $this-> showEditView("Try using a positive ticket value.", $id);
-            $this-> cinemaDAO-> Add($cinema);
-            $this-> removeCinema($id);
+            if ($this-> validateCinemaEdit($id, $name)){
+                $this-> showEditView($id, "Nombre de cine ya existente. Intente con otro.");
+            } else {
+                $cinema = new Cinema();
+                $cinema-> setName($name);
+                if ($ticketValue>=0) {
+                    $cinema-> setTicketValue($ticketValue);
+                    $this-> cinemaDAO-> Add($cinema);
+                    $this-> removeCinema($id);
+                } else {
+                    $this-> showEditView($id, "Intente usar un valor de entrada positivo.");
+                }
+            }
         }
       
         function showListView (){
@@ -62,6 +59,25 @@
         function showEditView ($id, $message = ""){
             $cinema = $this-> cinemaDAO-> getByID($id);
             require_once(VIEWS_PATH."cinema-edit.php");
+        }
+        
+        private function validateCinemaName($name){
+            $cinemasList = $this-> cinemaDAO-> GetAll();
+            foreach ($cinemasList as $cinema){
+                if ($cinema-> getName() === $name){
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        private function validateCinemaEdit($id, $name){
+            $cinema = new Cinema();
+            $cinema = $this-> cinemaDAO-> GetByID($id);
+            if ($cinema-> getName() != $name){
+                return $this-> validateCinemaName($name);
+            }
+            return false;
         }
 
     }
--- Views/cinema-edit.php
+++ Views/cinema-edit.php
@@ -3,12 +3,16 @@
 ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
+          <?php if ($message != ""){ ?>
+               <div class="alert alert-danger" role="alert"> <?php echo $message?> </div>
+          <?php } ?>
           <div class="container">
                <h2 class="mb-4">Editar Cine</h2>
                <form action="<?php echo FRONT_ROOT ?>Cinema/editCinema" method="post" class="bg-light-alpha p-5">
                     <div class="row">                         
                          <div class="col-lg-4">
                               <div class="form-group">
+                                   <input type="hidden" name="id" value="<?php echo $cinema-> getID()?>">
                                    <label for="">Nombre (actual: <?php echo $cinema-> getName()?>)</label>
                                    <input type="text" name="name" value="<?php echo $cinema-> getName()?>" class="form-control" required autofocus>
                               </div>
